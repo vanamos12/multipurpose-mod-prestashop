365904219e3908609ec23c93b6862e73e9334153
vanamos12 <pokatchoneng@yahoo.fr>
2023-04-29 11:41:27 +0100

    interact with database front end

--- controllers/front/task.php
+++ controllers/front/task.php
@@ -10,7 +10,12 @@ class MultipurposeTaskModuleFrontController extends ModuleFrontController{
 
     public function initContent(){
         parent::initContent();
-        $this->context->smarty->assign(array());
+        $this->context->smarty->assign(array(
+            'nb_products' => Db::getInstance()->getValue('SELECT COUNT(*) FROM `'._DB_PREFIX_.'product`'),
+            'categories' => Db::getInstance()->executeS('SELECT `name` FROM `'._DB_PREFIX_.'category_lang` WHERE `id_lang` = '.(int)$this->context->language->id),
+            'shop_name' => Configuration::get('PS_SHOP_NAME'),
+            'manufacturer' => Db::getInstance()->getRow('SELECT * FROM `'._DB_PREFIX_.'manufacturer`')
+        ));
         $this->setTemplate('module:multipurpose/views/templates/front/task.tpl');
     }
 }
--- views/templates/front/task.tpl
+++ views/templates/front/task.tpl
@@ -1,6 +1,16 @@
 {extends file='page.tpl'}
 {block name="content"}
-<div class="container">
-    My content
-</div>
+<ul>
+    <li>{l s='NUmber of products' mod='multipurpose'}&nbsp;{$nb_products}</li>
+    <li>
+        Categories :
+        <ul>
+            {foreach from=$categories item=$cat}
+                <li>{$cat['name']}</li>
+            {{/foreach}}
+        </ul>
+    </li>
+    <li>SHOP_NAME : {$shop_name}</li>
+    <li>{$manufacturer['name']}</li>
+</ul>
 {/block}
